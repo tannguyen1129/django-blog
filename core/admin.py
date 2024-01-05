e95649656cdc39f2ac749cf0fd86112e184c92a7
Son Tan <132370008+tannguyen1129@users.noreply.github.com>
2024-01-06 01:21:45 +0700

    core app and blog model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Blog
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fileds = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'author', 'status')
+    list_editable = ('is_featured',)
+    
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
+
+
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-from . import views
+from core import views
+
+app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
